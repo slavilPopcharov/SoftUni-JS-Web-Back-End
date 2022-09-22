40d6bfa79381662333cb4d3505293242c2bc674d
slavilPopcharov <slavitostz1991@gmail.com>
2022-09-22 11:45:47 +0300

    Params demo

--- expressDemo.js
+++ expressDemo.js
@@ -4,7 +4,26 @@ const app = express();
 
 //Action: At each endpoint a func is being executed
 app.get ('/', (req, res) => {
-res.send('Hi');
+res.send('Home Page');
 });
+
+app.get ('/catalog', (req, res) => {
+res.send('Catalog Page');
+
+});
+
+//Params
+const items = [];
+
+app.post ('/catalog/:itemName', (req, res) => {
+items.push(req.params);
+const item = req.params.itemName;
+
+req.status(201);
+res.send(`New item was added to the catalog: ${item}`);
+});
+
+
+
 
 app.listen(5000, () => console.log("Listening on port 5000..."));
