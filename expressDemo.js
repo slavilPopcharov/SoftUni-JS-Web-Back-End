180c19a337f5232c64fc9ab8f95b8d5051985a7e
slavilPopcharov <slavitostz1991@gmail.com>
2022-09-23 11:40:16 +0300

    Middlewares and static files notes

--- expressDemo.js
+++ expressDemo.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require("express"); //.Router()
 const app = express();
 
 const items = [];
@@ -23,10 +23,39 @@ app.post("/catalog/:itemName", (req, res) => {
 });
 
 //Redirect
-app.get ('/back-to-catalog', (req, res) => {
-res.redirect('/catalog');
-
+app.get("/back-to-catalog", (req, res) => {
+  res.redirect("/catalog");
 });
 
 //download or read files inline res.download("fileName.pdf"); res.sendFile();
+
+//Middlewares: has access to req,res cycle. Used in routing, auth, err handling...
+//There are third-party MWs like cookieParser
+// Every req wiff first pass throw this middleware
+app.use((req, res, next) => {
+  console.log("Time:" + Date.now());
+  next();
+});
+
+const limitedItems = ["Item1","Item2","Item3",];
+const itemsMiddleware = (req, res, next) =>{
+  req.items = limitedItems; 
+  next(); //Will point to the action!
+};
+
+//Will execute middleware befor the action. The MW is for this route only.
+app.get ('/limited-items',itemsMiddleware, (req, res) => {
+if (req.items.length > 0) {
+  res.send(req.items.join(", "));
+} else {
+  res.send("No avaible items.");
+} 
+});
+
+// Every req wiff first pass throw this middleware. Write before paths!!!
+app.use(itemsMiddleware);
+
+// Static files. On this route (/static) will search for the file in the publicFolder
+app.use('/static', express.static('publicFolder'));
+
 app.listen(5000, () => console.log("Listening on port 5000..."));
