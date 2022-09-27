81dc4f9f39ae8c6fae4a492a7097dd51043f6086
slavilPopcharov <slavitostz1991@gmail.com>
2022-09-27 18:04:25 +0300

    Bcrypt demo

--- index.js
+++ index.js
@@ -4,24 +4,42 @@ const app = express();
 app.engine('hbs', hbs.engine({ extname: 'hbs' }));
 app.set('view engine', 'hbs');
 
-//Setup
+//Express-Session Setup
 const expressSession = require("express-session");
 app.use(expressSession({
   secret: 'keyboard cat',
   resave: false,
   saveUninitialized: true,
   cookie: { secure: false }
-}))
-
-
+}));
 app.get ('/', (req, res) => {
   req.session.username = "hello"
 res.send('Home page');
 });
 
-app.get ('/s', (req, res) => {
+app.get ('/session', (req, res) => {
 res.send(req.session);
 });
+
+//Bcrypt setup
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const password = '123'
+const hashedPass = '$2b$10$BdlRjcUO7Ys.H.ATg/QaMeWXxVqNq83gBB73dY0cmT6TlJatua2Wa'
+//Hashing a password
+app.get ('/hash/:password', async(req, res) => {
+  const salt = await bcrypt.genSalt(saltRounds);
+  const hash = await bcrypt.hash(req.params.password, salt)
+  res.send(`password: ${req.params.password}, salt: ${salt}\nhashedPass: ${hash}`);
+});
+
+//Comparing entered from user with the hashed password
+app.get ('/compare/:password', async (req, res) => {
+const isVAlidPassword = await bcrypt.compare(req.params.password, hashedPass)
+isVAlidPassword ? res.send("Wellcome") : res.send("Wrong password!")
+});
+
+
 
 
 
